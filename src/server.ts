dea40e9b539a08594d966954dfc39bc087740e48
Vitária Moura <vim.saturnino@gmail.com>
2022-10-31 10:51:32 -0300

    refact: server code

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import express from "express";
-const sequelize = require('sequelize');
-const dotenv = require('dotenv').config();
-const cookieParser = require('cookie-parser');
+import Sequelize from 'sequelize';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes';
 import { db } from './database/db';
 
+const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+db.Sequelize.sync({ force: true }).then(() => {
+  console.log("db has been re sync");
+});
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, async () => {
   await db.sync();
