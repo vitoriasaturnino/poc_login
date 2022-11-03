8718a338c5ab56b840995940763508a05d1b4bf2
vitoriasaturnino <vim.saturnino@gmail.com>
2022-11-02 21:30:27 -0300

    fix: remove unused userName check

--- src/middlewares/UserAuth.ts
+++ src/middlewares/UserAuth.ts
@@ -3,34 +3,24 @@ import UserModel from '../model/UserModel';
 //Function to check if username or email already exist in the database
 //this is to avoid having two users with the same username and email
  const saveUser = async (req, res, next) => {
- //search the database to see if user exist
- try {
-   const username = await UserModel.findOne({
-     where: {
-       userName: req.body.userName,
-     },
-   });
-   //if username exist in the database respond with a status of 409
-   if (username) {
-     return res.json(409).send("username already taken");
-   }
+    //search the database to see if user exist
+    try {
+      //checking if email already exist
+      const emailcheck = await UserModel.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
 
-   //checking if email already exist
-   const emailcheck = await UserModel.findOne({
-     where: {
-       email: req.body.email,
-     },
-   });
+      //if email exist in the database respond with a status of 409
+      if (emailcheck) {
+        return res.json(409).send("Authentication failed");
+      }
 
-   //if email exist in the database respond with a status of 409
-   if (emailcheck) {
-     return res.json(409).send("Authentication failed");
-   }
-
-   next();
- } catch (error) {
-   console.log(error);
- }
+      next();
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 export default saveUser;
