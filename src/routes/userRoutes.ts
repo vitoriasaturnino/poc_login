2e9bb3bf89a64e58878214709421dabc3e684b53
vitoriasaturnino <vim.saturnino@gmail.com>
2022-11-02 21:32:42 -0300

    fix: add userAuth middleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,16 +1,14 @@
-//importing modules
 import express from 'express';
 import userController from '../controllers/UserController';
-import userAuth from '../middlewares/UserAuth';
+import UserAuth from '../middlewares/UserAuth';
 
 const { signup, login } = userController;
 const router = express.Router();
 
 //signup endpoint
 //passing the middleware function to the signup
-router.post('/signup', userAuth.saveUser, signup);
-
-//login route
+router.post('/signup', UserAuth, signup)
+//login routeb
 router.post('/login', login );
 
 export default router;
